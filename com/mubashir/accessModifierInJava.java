ecdc1c1947f9ad8e5651737faa26b1980934e0d1
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-13 02:33:09 +0500

    public class accessModifierInJava

---
+++ com/mubashir/accessModifierInJava.java
@@ -0,0 +1,33 @@
+package com.mubashir;
+
+class MyEmploye {
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setId(int i) {
+        id = i;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+public class accessModifierInJava {
+    public static void main(String[] args) {
+        MyEmploye Mubashir = new MyEmploye();
+        // Mubashir.id = 18;
+        // Mubashir.name = "Blockchain Scientist";
+        // trow error due to private acces modifier
+        Mubashir.setName("Blockchain Eng");
+        Mubashir.setId(12);
+    }
+}
