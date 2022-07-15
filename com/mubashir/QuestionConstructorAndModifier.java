8352bf34e648db9ea1a664e29b341d44fcbfab57
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-15 14:31:39 +0500

    QuestionConstructorAndModifier

--- com/mubashir/QuestionConstructorAndModifier.java
+++ com/mubashir/QuestionConstructorAndModifier.java
@@ -1,7 +1,15 @@
+
 package com.mubashir;
+
 class Cylinder {
     private int radius;
     private int height;
+
+    public Cylinder(int radius, int height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
     public int getRadius() {
         return radius;
     }
@@ -18,17 +26,58 @@ class Cylinder {
         this.height = height;
     }
 
+    public double surfaceArea() {
+        return 2 * Math.PI * radius * radius + 2 * Math.PI * radius * height;
+    }
+
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
+
 }
 
-/**
- * QuestionConstructorAndModifier
- */
+class Rectangle {
+    private int length;
+    private int breadth;
+
+    public Rectangle() {
+        this.length = 4;
+        this.breadth = 5;
+    }
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+}
 
 public class QuestionConstructorAndModifier {
 
     public static void main(String[] args) {
-        Cylinder  mCylinder = new Cylinder();
-        mCylinder.setHeight(12);
-        mCylinder.setRadius(8);
+        /*
+         * // Problem 1
+         * Cylinder myCylinder = new Cylinder(9, 12);
+         * //myCylinder.setHeight(12);
+         * System.out.println(myCylinder.getHeight());
+         * //myCylinder.setRadius(9);
+         * System.out.println(myCylinder.getRadius());
+         * // Problem 2
+         * System.out.println(myCylinder.surfaceArea());
+         * System.out.println(myCylinder.volume());
+         */
+
+        // Problem 3
+        Rectangle r = new Rectangle(12, 56);
+        System.out.println(r.getLength());
+        System.out.println(r.getBreadth());
+
     }
 }
