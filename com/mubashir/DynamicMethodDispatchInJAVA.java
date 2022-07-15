966a64f34729fa53eb54d19b8959cce7d7bced68
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-15 18:54:22 +0500

    Create DynamicMethodDispatchInJAVA.java

---
+++ com/mubashir/DynamicMethodDispatchInJAVA.java
@@ -0,0 +1,42 @@
+package com.mubashir;
+
+/**
+ * One
+ */
+class phone {
+    public void greed() {
+        System.out.println("Hello How are you !!!");
+    }
+
+    public void name() {
+        System.out.println("Hello My name Nokia 3310");
+    }
+}
+
+/**
+ * Two extends One
+ */
+class SmartPhone extends phone {
+    public void name() {
+        System.out.println("MY Name is Iphone X");
+    }
+
+    public void Salam() {
+        System.out.println("Hello Assalam - e - Alikum");
+    }
+}
+
+/**
+ * DynamicMethodDispatchInJAVA
+ */
+public class DynamicMethodDispatchInJAVA {
+    public static void main(String[] args) {
+        // phone obj = new phone();
+        // SmartPhone smobj = new SmartPhone();
+        // obj.name();
+        phone obj = new SmartPhone();
+        obj.greed();
+        obj.name();
+    }
+
+}
