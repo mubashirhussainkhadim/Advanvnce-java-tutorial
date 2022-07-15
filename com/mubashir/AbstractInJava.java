f5ccda994fe7e99a715fe3913be871f735a2e6f7
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-15 19:53:32 +0500

    Update AbstractInJava.java

--- com/mubashir/AbstractInJava.java
+++ com/mubashir/AbstractInJava.java
@@ -1,4 +1,3 @@
-// AbstractInJava
 package com.mubashir;
 
 /**
@@ -8,11 +7,14 @@ abstract class Parent2 {
     public Parent2() {
         System.out.println("I am A constructor of Base 2");
     }
+
     public void SayHello() {
         System.out.println("Hello");
     }
+
     abstract public void greed();
 }
+
 class Child2 extends Parent2 {
     @Override
     public void greed() {
@@ -20,12 +22,15 @@ class Child2 extends Parent2 {
         System.out.println("Good Morning");
     }
 }
+
 abstract class Child3 extends Parent2 {
     public void th() {
         System.out.println("I am good");
     }
 }
-public class AbstractInJava {
 
-    
+public class AbstractInJava {
+    // Parent2 A = new Child2();
+    // Parent2 B = new Child3()
+
 }
