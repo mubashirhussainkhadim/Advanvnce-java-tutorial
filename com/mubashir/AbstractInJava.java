b833f36d5f6d1edde77cc56a45663d70792cdb1f
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-15 21:31:47 +0500

    inheritenceinjava

---
+++ com/mubashir/AbstractInJava.java
@@ -0,0 +1,36 @@
+package com.mubashir;
+
+/**
+ * AbstractInJava
+ */
+abstract class Parent2 {
+    public Parent2() {
+        System.out.println("I am A constructor of Base 2");
+    }
+
+    public void SayHello() {
+        System.out.println("Hello");
+    }
+
+    abstract public void greed();
+}
+
+class Child2 extends Parent2 {
+    @Override
+    public void greed() {
+        // TODO Auto-generated method stub
+        System.out.println("Good Morning");
+    }
+}
+
+abstract class Child3 extends Parent2 {
+    public void th() {
+        System.out.println("I am good");
+    }
+}
+
+public class AbstractInJava {
+    // Parent2 A = new Child2();
+    // Parent2 B = new Child3()
+
+}
