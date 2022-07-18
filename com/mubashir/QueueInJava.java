23351bfbd87d85f89e344f08072b7b0a67a1c70e
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-18 22:28:45 +0500

    Create QueueInJava.java

---
+++ com/mubashir/QueueInJava.java
@@ -0,0 +1,54 @@
+package com.mubashir;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class QueueInJava {
+
+	public static void main(String[] args) {
+//     Queue<Integer> queue = new LinkedBlockingQueue<>();
+//     queue.add(1);
+//     queue.add(2);
+//     queue.add(3);
+//     queue.add(4);
+//     queue.add(5);
+//     queue.add(6);
+//     queue.add(5);
+//     queue.add(9);
+     Queue<Integer> queue = new ArrayBlockingQueue<>(6);
+     System.out.println(queue.poll());
+     try {
+      queue.offer(1);
+      queue.offer(2);
+      queue.offer(3);
+      queue.offer(4);
+      queue.offer(5);
+      queue.offer(6);
+      queue.offer(5);
+      queue.offer(1);
+      queue.offer(2);
+      queue.offer(3);
+      queue.offer(4);
+      queue.offer(5);
+      queue.offer(6);
+      queue.offer(5);
+      } catch (IllegalStateException e) {
+    	  System.out.println("Queue is full");
+      }
+
+      
+     
+     for (Integer e:queue) {
+    	 System.out.println(e);
+     }
+     System.out.println("***********************");
+     System.out.println(queue.remove());
+     System.out.println("***********************");
+     System.out.println(queue.remove());
+     System.out.println("***********************");
+    System.out.println(queue.poll());
+    System.out.println("First element in queue: "+queue.element());
+	}
+
+}
