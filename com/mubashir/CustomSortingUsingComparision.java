0b499d4f9445969325879e7122b787039f246eeb
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-18 21:10:01 +0500

    Update CustomSortingUsingComparision.java

--- com/mubashir/CustomSortingUsingComparision.java
+++ com/mubashir/CustomSortingUsingComparision.java
@@ -28,7 +28,24 @@ class Data<K,V>{
 
 public class CustomSortingUsingComparision {
     public  static void main(String[] args) {
-    	Comparator<Data<Integer,String>> COMPARE_KEY = new Comparator<Data<Integer,String>>(){
+    	Comparator<Data<Integer,String>> COMPARE_KEY_LENGTH = new Comparator<Data<Integer,String>>()
+    	{
+
+			@Override
+			public int compare(Data<Integer, String> obj1, Data<Integer, String> obj2) {				if (obj1.getValue().length() < obj2.getValue().length()) {
+					return -1;
+				}
+				else if (obj1.getValue().length() > obj2.getValue().length()) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+    		
+    	};
+    	
+    	Comparator<Data<Integer,String>> COMPARE_KEY = new Comparator<Data<Integer,String>>()
+    	{
 
 			@Override
 			public int compare(Data<Integer, String> obj1, Data<Integer, String> obj2) {
@@ -42,12 +59,17 @@ public class CustomSortingUsingComparision {
 			}
     		
     	};
-    	Set<Data<Integer, String>> set = new TreeSet<>(COMPARE_KEY);
-    	set.add(new Data<Integer, String>(1, "one"));
-    	set.add(new Data<Integer, String>(2, "two"));
-    	set.add(new Data<Integer, String>(3, "three"));
-    	set.add(new Data<Integer, String>(4, "four"));
-    	set.add(new Data<Integer, String>(5, "five"));
+    	Set<Data<Integer, String>> set = new TreeSet<>(COMPARE_KEY_LENGTH);
+//    	set.add(new Data<Integer, String>(1, "one"));
+//    	set.add(new Data<Integer, String>(2, "two"));
+//    	set.add(new Data<Integer, String>(3, "three"));
+//    	set.add(new Data<Integer, String>(4, "four"));
+//    	set.add(new Data<Integer, String>(5, "five"));
+    	set.add(new Data<Integer, String>(5, "Mubashirhussain"));
+    	set.add(new Data<Integer, String>(3, "Hussain"));
+    	set.add(new Data<Integer, String>(4, "Husain"));
+    	set.add(new Data<Integer, String>(2, "ADNAM"));
+    	set.add(new Data<Integer, String>(1, "QASIM khadim"));
          
     	for (Data<Integer, String> element:set) {
     		System.out.println(element);
